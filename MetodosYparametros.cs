9e580b72200d2f69d5f20ee8a406175d0c23f39c
Yhavet Alaniz <104223938+yhavet@users.noreply.github.com>
2023-12-14 15:35:58 -0300

    Add files via upload

---
+++ MetodosYparametros.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Metodos_Y_Parametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumarNumeros(7,9);
+        }
+
+        static void SumarNumeros(int num1, int num2)
+        {
+
+            Console.WriteLine("Ingrese el primer parametro");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo parametro parametro");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("parametro sumando" + (num1+num2 * num2 / 5));
+
+        }
+    }
+}
+
+
