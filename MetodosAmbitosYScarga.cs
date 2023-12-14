aebe883abc2220cffc1aae51ee76ad63ad1d1035
Yhavet Alaniz <104223938+yhavet@users.noreply.github.com>
2023-12-14 17:19:57 -0300

    Update MetodosAmbitosYScarga.cs

--- MetodosAmbitosYScarga.cs
+++ MetodosAmbitosYScarga.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace MetodosAmbitosYScarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Suma(3, 5));
+        }
+
+        static int Suma(int op1, int op2) => op1 + op2;
+
+        static int Suma(int op1, double op2) => op1 ;
+
+        static int Suma(int op1, int op2, int op3) => op1 + op2 / op3;
+    }
+}
+
